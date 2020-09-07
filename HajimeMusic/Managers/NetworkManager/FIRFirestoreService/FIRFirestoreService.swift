60b3c5bcc27748d943f2fcb6bc0380a96b00b229
Aleksandr <bagdasaryanaleksandr97@mail.ru>
2020-09-07 19:16:39 +0400

    For Demo.

--- HajimeMusic/Managers/NetworkManager/FIRFirestoreService/FIRFirestoreService.swift
+++ HajimeMusic/Managers/NetworkManager/FIRFirestoreService/FIRFirestoreService.swift
@@ -6,78 +6,75 @@
 //  Copyright © 2020 Aleksandr Bagdasaryan. All rights reserved.
 //
 
-import Foundation
-import FirebaseFirestore
+//import Foundation
+
+//enum NetworkError: String, Error {
+//    case encodingError = "Encoding is failed"
+//    case decogingError = "Decoding is failed"
+//    case ubsenceId     = "User id is ubsent"
+//    case emptySnapshot = "Snapshot is nil"
+//}
+
+//enum FIRCollectionReference: String {
+//    case music
+//}
 
 
-
-enum NetworkError: String, Error {
-    case encodingError = "Encoding is failed"
-    case decogingError = "Decoding is failed"
-    case ubsenceId     = "User id is ubsent"
-    case emptySnapshot = "Snapshot is nil"
-}
-
-enum FIRCollectionReference: String {
-    case music
-}
-
-
-protocol FirestoreService {
-    func read<T: Decodable>(from collectionReference: FIRCollectionReference, returning objectType: T.Type, completion: @escaping([T]) -> Void)
-}
-
-class FIRFirestoreService: FirestoreService {
-    
-    //MARK: - Properties -
-    
-    static let shared = FIRFirestoreService()
-    
-    
-    
-    //MARK: - Init -
-    
-    private init() {}
-    
-    
-    
-    //MARK: - Requests -
-    
-    func read<T: Decodable>(from collectionReference: FIRCollectionReference, returning objectType: T.Type, completion: @escaping([T]) -> Void) {
-        reference(to: collectionReference).addSnapshotListener { (snapshot, _) in
-            guard let snapshot = snapshot else { return }
-            do {
-                var objects = [T]()
-                for document in snapshot.documents {
-                    let object = try document.decode(as: objectType.self)
-                    objects.append(object)
-                }
-                completion(objects)
-            } catch {
-                print(error)
-            }
-        }
-    }
-    
-    
-    //MARK: - Helpers -
-    
-    private func reference(to collectionReference: FIRCollectionReference) -> CollectionReference {
-        return Firestore.firestore().collection(collectionReference.rawValue)
-    }
-}
-
-
-extension DocumentSnapshot {
-    func decode<T: Decodable>(as objectType: T.Type, includingId: Bool = true) throws -> T {
-        var documentJson = data()
-        if includingId {
-            documentJson!["id"] = documentID
-        }
-        
-        let documentData = try JSONSerialization.data(withJSONObject: documentJson!, options: [])
-        let decodedObject = try JSONDecoder().decode(objectType, from: documentData)
-        
-        return decodedObject
-    }
-}
+//protocol FirestoreService {
+//    func read<T: Decodable>(from collectionReference: FIRCollectionReference, returning objectType: T.Type, completion: @escaping([T]) -> Void)
+//}
+//
+//class FIRFirestoreService: FirestoreService {
+//
+//    //MARK: - Properties -
+//
+//    static let shared = FIRFirestoreService()
+//
+//
+//
+//    //MARK: - Init -
+//
+//    private init() {}
+//
+//
+//
+//    //MARK: - Requests -
+//
+//    func read<T: Decodable>(from collectionReference: FIRCollectionReference, returning objectType: T.Type, completion: @escaping([T]) -> Void) {
+//        reference(to: collectionReference).addSnapshotListener { (snapshot, _) in
+//            guard let snapshot = snapshot else { return }
+//            do {
+//                var objects = [T]()
+//                for document in snapshot.documents {
+//                    let object = try document.decode(as: objectType.self)
+//                    objects.append(object)
+//                }
+//                completion(objects)
+//            } catch {
+//                print(error)
+//            }
+//        }
+//    }
+//
+//
+//    //MARK: - Helpers -
+//
+//    private func reference(to collectionReference: FIRCollectionReference) -> CollectionReference {
+//        return Firestore.firestore().collection(collectionReference.rawValue)
+//    }
+//}
+//
+//
+//extension DocumentSnapshot {
+//    func decode<T: Decodable>(as objectType: T.Type, includingId: Bool = true) throws -> T {
+//        var documentJson = data()
+//        if includingId {
+//            documentJson!["id"] = documentID
+//        }
+//
+//        let documentData = try JSONSerialization.data(withJSONObject: documentJson!, options: [])
+//        let decodedObject = try JSONDecoder().decode(objectType, from: documentData)
+//
+//        return decodedObject
+//    }
+//}
